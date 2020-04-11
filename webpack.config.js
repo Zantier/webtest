0bfeb3ebe91a274b3dee0e494cb32868bd6e400d
David Phillips <zantier.tasa@gmail.com>
2020-04-11 13:01:07 +0100

    Compile js to ES5 with @babel/preset-env
    If we need polyfills (e.g. Promise and Set), need to use core-js and the
    "useBuiltIns" option in @babel/preset-env.

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,7 @@ module.exports = {
   module: {
     rules: [
       { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
     ],
   },
   plugins: [
